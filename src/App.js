9ae2e701192faf9c178443b31a63dc6337bd8bf0
代胜全 <945531626@qq.com>
2019-05-07 17:04:01 +0800

    第一次提交

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Button } from './components';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button text={'sda'}></Button>
+      </div>
+    );
+  }
+}
+
+export default App;
+
